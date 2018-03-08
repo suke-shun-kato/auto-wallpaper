182c4542f30b708761230a40f36ec51a56dd3291
Kato Shunsuke <suke.shuh.kato@gmail.com>
2018-03-08 10:49:54 +0900

    feat: Aboutページにバージョン番号をひとまず表示

--- app/src/main/java/xyz/monogatari/autowallpaper/AboutActivity.java
+++ app/src/main/java/xyz/monogatari/autowallpaper/AboutActivity.java
@@ -1,9 +1,12 @@
 package xyz.monogatari.autowallpaper;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * ライセンス表示用のアクティビティ
@@ -18,10 +21,36 @@ public class AboutActivity extends AppCompatActivity {
         // ----------------------------------
         // アクションバーの設定
         // ----------------------------------
+        // todo アクションバーの戻るボタンが効かない不具合を修正
         // ここのActionBar は android.support.v7.app.ActionBa の方のクラスになる
         ActionBar actionBar = this.getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        // ----------------------------------
+        // バージョンのテキストを動的にセット
+        // ----------------------------------
+        TextView tx = this.findViewById(R.id.about_version);
+        tx.setText(
+                String.format(getString(R.string.about_version), this.getVersionName())
+        );
+
+    }
+
+    /**
+     * バージョン名を取得する
+     *
+     * @return バージョン名
+     */
+    private String getVersionName() {
+        String versionName = "";
+        try {
+            PackageInfo packageInfo = this.getPackageManager().getPackageInfo(this.getPackageName(), 0);
+            versionName = packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionName;
     }
 }
