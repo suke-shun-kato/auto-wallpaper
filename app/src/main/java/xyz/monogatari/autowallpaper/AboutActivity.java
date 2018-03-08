0c15d9e3843047286c961b2c618c204c4d0d92ce
Kato Shunsuke <suke.shuh.kato@gmail.com>
2018-03-08 13:45:53 +0900

    feat: Aboutページ、アクションバーの戻るボタンを押したとき前画面に戻るよ機能を実装

--- app/src/main/java/xyz/monogatari/autowallpaper/AboutActivity.java
+++ app/src/main/java/xyz/monogatari/autowallpaper/AboutActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,6 +16,8 @@ import android.widget.TextView;
  * Created by k-shunsuke on 2018/03/07.
  */
 public class AboutActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +26,6 @@ public class AboutActivity extends AppCompatActivity {
         // ----------------------------------
         // アクションバーの設定
         // ----------------------------------
-        // todo アクションバーの戻るボタンが効かない不具合を修正
         // ここのActionBar は android.support.v7.app.ActionBa の方のクラスになる
         ActionBar actionBar = this.getSupportActionBar();
         if (actionBar != null) {
@@ -49,6 +51,19 @@ public class AboutActivity extends AppCompatActivity {
         );
         lv.setAdapter(adapter);
 
+    }
+
+    /************************************
+     * ボタンを押したときの制御
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home: //押したボタンがアクションバーの←ボタンだとすると
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
